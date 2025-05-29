a39a3286309b632f42d52777f6f3a004c66855ef
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-05-29 22:52:05 +0300

    add pdf download route

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import CVListView, CVDetailView
+from main.views import CVListView, CVDetailView, export_cv_pdf
 
 urlpatterns = [
     path("", CVListView.as_view(), name="cv_list"),
     path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
+    path("cv/<int:pk>/pdf/", export_cv_pdf, name="cv_pdf"),
 ]
 
 app_name = "main"
