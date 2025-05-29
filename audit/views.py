a05435182ccd47fb1a2f9c11096aa67b4ff6b59b
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-05-30 00:40:48 +0300

    add logging view

--- audit/views.py
+++ audit/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from audit.models import RequestLog
+
+
+def recent_logs_view(request):
+    logs = RequestLog.objects.order_by("-timestamp")[:10]
+    return render(
+        request,
+        "audit/recent_logs.html",
+        {"logs": logs}
+    )
