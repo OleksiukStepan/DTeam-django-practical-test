ac089ba734604f1c515efc9eca35c8cff8890ae9
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-05-30 00:13:01 +0300

    add app audit

--- CVProject/settings.py
+++ CVProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     # my apps
     "main",
+    "audit",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "audit.middleware.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "CVProject.urls"
