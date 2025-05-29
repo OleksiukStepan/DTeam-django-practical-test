55655a864c9f4243e91187f75409444a29a01b61
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-05-29 20:47:34 +0300

    add CVListView

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from main.models import CV
 
-# Create your views here.
+
+class CVListView(ListView):
+    model = CV
+    template_name = "main/cv_list.html"
+    context_object_name = "cv_list"
+    queryset = CV.objects.select_related("contacts").prefetch_related(
+        "skills", "projects"
+    )
